9dd4eb59a8f263b8e3f65a15588041e3fe1c0446
Ztaclie <melmeh42@gmail.com>
2023-12-06 12:47:54 +0300

    added variables and showed them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 const App = () => {
+    const name = "Samantha";
+    const age = 25;
+
     return (
         <div className="App">
-            <p>App</p>
+            <p>My Name is {name}, my age is {age}</p>
         </div>
     )
 };
